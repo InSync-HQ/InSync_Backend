fae3600f67a131e395f42038e8807a65dce678ac
abishek982001 <abishek982001@gmail.com>
2022-04-16 23:31:53 +0530

    feat/v1.0.0

--- src/db/repositories/CommunityRepo.ts
+++ src/db/repositories/CommunityRepo.ts
@@ -2,11 +2,17 @@ import { CommunityModel, ICommunity } from "../../models/Community";
 
 
 export class CommunityRepo {
-    public async create(community: ICommunity) {
-        try {
-            const createdCommunity = await CommunityModel.create(community);
-        } catch (err) {
-            return err;
-        }
+    public async create(community: ICommunity): Promise<ICommunity> {
+        const createdCommunity = await CommunityModel.create(community);
+        return createdCommunity.toObject() as ICommunity;
+    }
+
+    public async update(community: ICommunity): Promise<any> {
+        return CommunityModel.updateOne({ _id: community._id }, { ...community })
+            .lean<ICommunity>()
+            .exec();
+    }
+    public async findById(_id: string): Promise<ICommunity> {
+        return CommunityModel.findById(_id).lean<ICommunity>().exec();
     }
 }
