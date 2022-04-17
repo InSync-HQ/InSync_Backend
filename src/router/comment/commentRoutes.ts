f2695337d26b5dfce647667cba90dbfc6f346cc9
madrigal1 <kurupgokul11@gmail.com>
2022-04-17 17:21:01 +0530

    feat/comment-crud v1.1.0

--- src/router/comment/commentRoutes.ts
+++ src/router/comment/commentRoutes.ts
@@ -9,7 +9,7 @@ const router = express.Router();
 router.use("/", passport.authenticate("jwt", { session: false }))
 
 router.post("/new", validator(commentSchema.new), createComment);
-router.get("/forArticle", fetchCommentsForArticle);
+router.get("/forArticle/:article_id", fetchCommentsForArticle);
 router.patch("/update/:id", validator(commentSchema.update), updateComment);
 
 export default router;
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,11 +3,12 @@ import userRoutes from "./user/userRoutes";
 import fetchNewsRoutes from './fetchNewsRoutes'
 import communityRoutes from './community/communityRoutes'
 import articleRoutes from './article/articleRoutes'
-
+import commentRoutes from './comment/commentRoutes'
 const router = express.Router();
 
 router.use("/user", userRoutes);
 router.use("/news/newsapi", fetchNewsRoutes);
 router.use("/community", communityRoutes);
 router.use("/article", articleRoutes);
+router.use("/comment", commentRoutes);
 export default router;
